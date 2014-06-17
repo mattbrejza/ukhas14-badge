a314599103a170dd35a834fd5eab99eb4ec12409
Jon Sowman <jon@hexoc.com>
2014-06-17 22:14:20 +0100

    First go at locm3 firmware

---
+++ firmware/src/badge.d
@@ -0,0 +1,16 @@
+badge.o: badge.c ../libopencm3//include/libopencm3/stm32/rcc.h \
+ ../libopencm3//include/libopencm3/cm3/common.h \
+ /Users/js39g13/code/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
+ /Users/js39g13/code/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h \
+ /Users/js39g13/code/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/js39g13/code/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h \
+ /Users/js39g13/code/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdbool.h \
+ ../libopencm3//include/libopencm3/stm32/memorymap.h \
+ ../libopencm3//include/libopencm3/stm32/f0/memorymap.h \
+ ../libopencm3//include/libopencm3/cm3/memorymap.h \
+ ../libopencm3//include/libopencm3/stm32/f0/rcc.h \
+ ../libopencm3//include/libopencm3/stm32/gpio.h \
+ ../libopencm3//include/libopencm3/stm32/f0/gpio.h \
+ ../libopencm3//include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3//include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3//include/libopencm3/stm32/common/gpio_common_all.h
