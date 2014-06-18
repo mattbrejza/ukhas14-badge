4d2bfc91d94ae7034c712e0f277810359a378400
Jon Sowman <jon@jonsowman.com>
2014-06-18 10:16:11 +0100

    Defines mostly done for the LCD, code is a bit nicer

--- firmware/src/lcd.c
+++ firmware/src/lcd.c
@@ -60,45 +60,59 @@ void lcd_init(void)
     _delay_ms(200);
 
     // Run through the init routine given in the datasheet
-    lcd_write_instruction(0xae);//--turn off oled panel
+    lcd_write_instruction(LCD_DISPLAY_OFF);
 
-    lcd_write_instruction(0xd5);//--set display clock divide ratio/oscillator frequency
-    lcd_write_instruction(0x80);//--set divide ratio
+    // Set oscillator frequency to 0b1000 and divider to 0b0000
+    lcd_write_instruction(LCD_CKDIV_OSC_FREQ);
+    lcd_write_instruction(0x80);
 
-    lcd_write_instruction(0xa8);//--set multiplex ratio(1 to 64)
-    lcd_write_instruction(0x1f);//--1/32 duty
+    // Set multiplex ratio to 1/32 duty (0x1F)
+    lcd_write_instruction(LCD_MUX_RATIO);
+    lcd_write_instruction(0x1f);
 
-    lcd_write_instruction(0xd3);//-set display offset
-    lcd_write_instruction(0x00);//-not offset
+    // Set LCD to no offset
+    lcd_write_instruction(LCD_DISPLAY_OFFSET);
+    lcd_write_instruction(0x00);
 
+    // Enable the charge pump to generate Vcc internally
+    lcd_write_instruction(LCD_SET_CHARGEPUMP);
+    lcd_write_instruction(LCD_CHARGEPUMP_ON);
 
-    lcd_write_instruction(0x8d);//--set Charge Pump enable/disable
-    lcd_write_instruction(0x14);//--set(0x10) disable
-
-    lcd_write_instruction(0x40);//--set start line address
-
-    lcd_write_instruction(0xa6);//--set normal display
-
-    lcd_write_instruction(0xa4);//Disable Entire Display On
-
-    lcd_write_instruction(0xa1);//--set segment re-map 128 to 0
-
-    lcd_write_instruction(0xC8);//--Set COM Output Scan Direction 64 to 0
-
-    lcd_write_instruction(0xda);//--set com pins hardware configuration
-    lcd_write_instruction(0x42);
-
-    lcd_write_instruction(0x81);//--set contrast control register
-    lcd_write_instruction(0xcf);
-
-
-    lcd_write_instruction(0xd9);//--set pre-charge period
-    lcd_write_instruction(0xf1);
-
-    lcd_write_instruction(0xdb);//--set vcomh
+    // Set start line address (0b01xxxxxx where 6 LSBs are the line 0-63)
     lcd_write_instruction(0x40);
 
-    lcd_write_instruction(0xaf);//--turn on oled panel
+    // Use normal mode (rather than inverted display)
+    lcd_write_instruction(LCD_SET_NORMAL_MODE);
+
+    // Disable ENTIRE ON (overrides the RAM buffer and turns whole screen on)
+    lcd_write_instruction(LCD_ENTIRE_DISPLAY_ON_DISABLE);
+
+    // Remap column 127 to segment 0
+    lcd_write_instruction(LCD_SEG_REMAP_COL127_SEG0);
+
+    // Change the output scan direction to scan from COM[N-1] to COM0
+    lcd_write_instruction(LCD_COM_SCAN_DIR_REVERSE);
+
+    // Set COM pins hardware configuration to SEQUENTIAL
+    lcd_write_instruction(LCD_COM_PINS_CONFIG);
+    lcd_write_instruction(0x42); //why this number? should be 0x02
+
+    // Change the contrast
+    lcd_write_instruction(LCD_SET_CONTRAST);
+    lcd_write_instruction(0xcf); // this number came from the datasheet and appears to look ok
+
+    // Set pre-charge period (upper nibble is 0b1111, lower is 0b0001) - from
+    // datasheet
+    lcd_write_instruction(LCD_PRECHARGE_PERIOD);
+    lcd_write_instruction(0xf1);
+
+    // Sert deselect level. 0x40 comes from datasheet and appears to be an
+    // invalid setting! (Valid are 0x00, 0x20 or 0x30)
+    lcd_write_instruction(LCD_VCOMH_DESEL_LVL);
+    lcd_write_instruction(0x40);
+
+    // Finally, turn the display on
+    lcd_write_instruction(LCD_DISPLAY_ON);
 }
 
 /**
--- firmware/src/lcd.h
+++ firmware/src/lcd.h
@@ -18,6 +18,44 @@
 #define LCD_MOSI        GPIO7
 #define LCD_SCK         GPIO5
 
+/* Fundamental commands */
+#define LCD_SET_CONTRAST                    0x81
+#define LCD_ENTIRE_DISPLAY_ON_ENABLE        0xA5
+#define LCD_ENTIRE_DISPLAY_ON_DISABLE       0xA4
+#define LCD_SET_NORMAL_MODE                 0xA6
+#define LCD_SET_INVERSE_MODE                0xA7
+#define LCD_DISPLAY_OFF                     0xAE
+#define LCD_DISPLAY_ON                      0xAF
+
+/* TODO: Scrolling command table */
+
+/* Addressing setting commands */
+#define LCD_MEM_ADDRESSING_MODE             0x20
+#define LCD_COL_ADDRESS                     0x21
+#define LCD_PAGE_ADDRESS                    0x22
+
+/* Hardware configuration commands */
+#define LCD_SEG_REMAP_COL0_SEG0             0xA0
+#define LCD_SEG_REMAP_COL127_SEG0           0xA1
+#define LCD_MUX_RATIO                       0xA8
+#define LCD_COM_SCAN_DIR_NORMAL             0xC0
+#define LCD_COM_SCAN_DIR_REVERSE            0xC8
+#define LCD_DISPLAY_OFFSET                  0xD3
+#define LCD_COM_PINS_CONFIG                 0xDA
+
+/* Timing and driving scheme setting commands */
+#define LCD_CKDIV_OSC_FREQ                  0xD5
+#define LCD_PRECHARGE_PERIOD                0xD9
+#define LCD_VCOMH_DESEL_LVL                 0xDB
+#define LCD_NOP                             0xE3
+
+/* TODO: Advanced graphic command table */
+
+/* Charge pump commands */
+#define LCD_SET_CHARGEPUMP                  0x8D
+#define LCD_CHARGEPUMP_OFF                  0x10
+#define LCD_CHARGEPUMP_ON                   0x14
+
 void lcd_init(void);
 
 #endif /* __UKHAS_LCD_H__ */
