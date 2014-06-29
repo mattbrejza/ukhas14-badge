f65738926c69847665e5e6bc0228600b81ba8a46
unknown <mbrejza@gmail.com>
2014-06-29 23:48:25 +0100

    minor changes (might have fixed ftdi in this one)

--- firmware/src/badge.c
+++ firmware/src/badge.c
@@ -8,12 +8,17 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/common/timer_common_all.h>
 
 #include "badge.h"
 #include "lcd.h"
 
+void init(void);
+
 int main(void)
 {
+    init();
     lcd_init();
     lcd_clear();
     lcd_set_display_ptr(2,0,3,127);
@@ -22,10 +27,40 @@ int main(void)
     /* Blink the LED (PC8) on the board. */
     while (1){
         _delay_ms(100);
+
+        //gpio_set(GPIOF, GPIO1);
+        //_delay_ms(1000);
+
+       //gpio_clear(GPIOF, GPIO1);
+        //}
+        
+        if (!(GPIOF_IDR & GPIO1))
+        	lcd_write_string("A");
 		
 		}
 
     return 0;
+}
+
+void init(void)
+{
+    rcc_periph_clock_enable(RCC_GPIOF);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOF, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO1);
+    
+    rcc_periph_clock_enable(RCC_TIM3);
+    timer_reset(TIM3);
+    timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT_MUL_2,
+               TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    gpio_set_af(GPIOB, GPIO_AF1, GPIO1);
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO1);
+              
+    timer_set_oc_mode(TIM3, TIM_OC4, TIM_OCM_PWM2);
+    timer_enable_oc_output(TIM3, TIM_OC4);
+    timer_set_oc_value(TIM3, TIM_OC4, 200);
+    timer_set_period(TIM3, 1000);
+    timer_enable_counter(TIM3);
+
 }
 
 /**
